da18a0ed0c4e5bfa64aaa8739f6b17304c909b5d
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2023-06-05 00:08:23 +0300

    edit class

--- lib/features/news_home/cubit/news_home_state.dart
+++ lib/features/news_home/cubit/news_home_state.dart
@@ -1,10 +1,27 @@
 part of 'news_home_cubit.dart';
 
-abstract class NewsHomeState extends Equatable {
-  const NewsHomeState();
+class NewsHomeState extends Equatable {
+  const NewsHomeState({
+    this.newsModel,
+    this.isLoading,
+    this.isInitial = false,
+  });
 
+  final NewsModel? newsModel;
+  final bool? isLoading;
+  final bool isInitial;
   @override
-  List<Object> get props => [];
-}
+  List<Object?> get props => [newsModel, isLoading];
 
-class NewsHomeInitial extends NewsHomeState {}
+  NewsHomeState copyWith({
+    NewsModel? newsModel,
+    bool? isLoading,
+    bool? isInitial,
+  }) {
+    return NewsHomeState(
+      newsModel: newsModel ?? this.newsModel,
+      isLoading: isLoading ?? this.isLoading,
+      isInitial: isInitial ?? this.isInitial,
+    );
+  }
+}
