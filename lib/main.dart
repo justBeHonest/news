9b70ecbc7777d6d431e1cb1f8123e9474868ad69
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2023-06-05 00:07:21 +0300

    change theme and add product navigate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:news/features/news_home/view/news_home.dart';
+import 'package:news/features/news_home/view/news_home_view.dart';
+import 'package:news/product/utility/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      theme: ThemeData.dark(),
-      home: NewsHome(),
+      home: NewsHomeView(),
+      theme: AppThemeData.lightTheme,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
