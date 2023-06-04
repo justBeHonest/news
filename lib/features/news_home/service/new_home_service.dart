52cb5ceb5dd0ec670a87013693b46dc01e53ef8f
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2023-06-05 00:08:49 +0300

    service for requests

---
+++ lib/features/news_home/service/new_home_service.dart
@@ -0,0 +1,48 @@
+import 'package:news/features/news_home/model/news_model.dart';
+import 'package:news/product/constant/application_constant.dart';
+import 'package:news/product/model/query/news_queries.dart';
+import 'package:vexana/vexana.dart';
+
+enum _NewsHomeServicePath { v2, everything }
+
+abstract class INewsHomeService {
+  late final INetworkManager _networkManager;
+  INewsHomeService(INetworkManager networkManager)
+      : _networkManager = networkManager;
+  // Future<List<ProductModel>?> fetchAllProducts({int count = 5});
+  // Future<List<String>?> fetchAllCategories();
+  Future<NewsModel?> fetchAllNews(
+      {required String q, int? pageSize, int? page, String? apiKey});
+}
+
+class NewsHomeService extends INewsHomeService {
+  NewsHomeService(INetworkManager networkManager) : super(networkManager);
+
+  @override
+  Future<NewsModel?> fetchAllNews(
+      {required String q, int? pageSize, int? page, String? apiKey}) async {
+    final response = await _networkManager.send<NewsModel, NewsModel>(
+      ('${_NewsHomeServicePath.v2.name}/${_NewsHomeServicePath.everything.name}'),
+      parseModel: NewsModel(),
+      method: RequestType.GET,
+      queryParameters: Map.fromEntries(
+        [
+          NewsQueries.apiKey
+              .toMapEntry(apiKey: apiKey ?? ApplicationConstant.instane.apiKey),
+          NewsQueries.q.toMapEntry(q: q),
+          NewsQueries.pageSize.toMapEntry(pageSize: pageSize ?? 10),
+          NewsQueries.page.toMapEntry(page: page ?? 1),
+        ],
+      ),
+    );
+    return response.data;
+  }
+
+  // @override
+  // Future<List<String>?> fetchAllCategories() async {
+  //   final response = await _networkManager.sendPrimitive(
+  //       '${_NewsHomeServicePath.products.name}/${_NewsHomeServicePath.categories.name}');
+  //   return response is List ? response.map((e) => '$e').toList() : null;
+  //   // https://fakestoreapi.com/products/categories
+  // }
+}
